e476304edbaa0bfa04085966f81f1c1afaec042c
Brad Ebinger <breadley@google.com>
2019-09-04 08:41:23 -0700

    Enable FEATURE_TELEPHONY_IMS for this device.
    Bug: 118823723
    Test: checkbuild
    Merged-In: Ic026d63d9bf7b59e2501d095799508d7232cad3f
    Change-Id: Ic026d63d9bf7b59e2501d095799508d7232cad3f

--- device.mk
+++ device.mk
@@ -145,6 +145,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:system/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:system/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml \
